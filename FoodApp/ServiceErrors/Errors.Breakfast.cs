7a02f51f25a5075ffd669705ded7469a7025b0d0
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2024-12-17 12:08:52 +0530

    fix

--- FoodApp/ServiceErrors/Errors.Breakfast.cs
+++ FoodApp/ServiceErrors/Errors.Breakfast.cs
@@ -11,13 +11,13 @@ public static class Errors {
 
         public static Error InvalidName => Error.Validation(
             code: "Breakfast.InvalidName",
-            description: $"Breakfast name must be at least {Models.Breakfast.MinNameLength}" +
+            description: $"Breakfast name must be at least {Models.Breakfast.MinNameLength} " +
             $"and at most {Models.Breakfast.MaxNameLength} characters long"
         );
 
         public static Error InvalidDescription => Error.Validation(
             code: "Breakfast.InvalidDescription",
-            description: $"Breakfast description must be at least {Models.Breakfast.MinDescriptionLength}" +
+            description: $"Breakfast description must be at least {Models.Breakfast.MinDescriptionLength} " +
             $"and at most {Models.Breakfast.MaxDescriptionLength} characters long"
         );
     }
